b39125c85d8b9e3e60ac7fea29bc05fc33d0cb4a
Nhan Nguyen <ngtrnhan1205@gmail.com>
2019-05-22 22:05:55 -0400

    Add themes

---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  accentColor: Colors.orangeAccent[400],
+);
