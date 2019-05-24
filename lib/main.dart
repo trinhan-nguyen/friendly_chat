acea08d6f24be26a1b8b5b9c66de687e4ee74523
Nhan Nguyen <ngtrnhan1205@gmail.com>
2019-05-23 23:15:24 -0400

    Apply finishing touches

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:friendlychat/model/chat_message.dart';
 import 'package:friendlychat/theme/theme_data.dart';
+
+const title = "Friendly Chat";
 
 void main() {
   runApp(new FriendlyChatApp());
@@ -11,7 +14,7 @@ class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Friendly Chat",
+      title: title,
       theme: defaultTargetPlatform == TargetPlatform.iOS
           ? kIOSTheme
           : kDefaultTheme,
@@ -52,7 +55,13 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
               ),
               new Container(
                   margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                  child: new IconButton(
+                  child: Theme.of(context).platform == TargetPlatform.iOS ?
+                  CupertinoButton(
+                    child: Text("Send"),
+                    onPressed: _isComposing
+                        ? () =>  _handleSubmitted(_textController.text)
+                        : null,) :
+                  IconButton(
                       icon: new Icon(Icons.send),
                       onPressed: _isComposing
                           ? () => _handleSubmitted(_textController.text)
@@ -88,24 +97,36 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Friendlychat")),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-            child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+      appBar: AppBar(
+        title: Text(title),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
             ),
-          ),
-          new Divider(height: 1.0),
-          new Container(
-            decoration: new BoxDecoration(
-                color: Theme.of(context).cardColor),
-            child: _buildTextComposer(),
-          ),
-        ],
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+          ? BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey[200]),
+            )
+          )
+          : null,
       ),
     );
   }
