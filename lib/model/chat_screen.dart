49d2222172331dd1cd3235daac23df52dcb0e41c
Nhan Nguyen <ngtrnhan1205@gmail.com>
2019-05-26 16:20:32 -0400

    Refactor some code

---
+++ lib/model/chat_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:friendlychat/model/chat_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  final _title;
+
+  ChatScreen(String title): _title = title;
+
+  @override
+  State createState() => ChatScreenState(_title);
+}
+
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final _title;
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+  bool _isComposing = false;
+
+  ChatScreenState(String title): _title = title;
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onChanged: (String text) {
+                    setState(() {
+                      _isComposing = text.length > 0;
+                    });
+                  },
+                  onSubmitted: _handleSubmitted,
+                  decoration: InputDecoration.collapsed(
+                      hintText: "Send a message"),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Theme.of(context).platform == TargetPlatform.iOS ?
+                  CupertinoButton(
+                    child: Text("Send"),
+                    onPressed: _isComposing
+                        ? () =>  _handleSubmitted(_textController.text)
+                        : null,) :
+                  IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: _isComposing
+                        ? () => _handleSubmitted(_textController.text)
+                        : null,
+                  )
+              )
+            ],
+          ),
+        )
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+
+    setState(() {
+      _isComposing = false;
+    });
+
+    ChatMessage message = ChatMessage(
+      text: text,
+      animationController: AnimationController(
+        duration: Duration(milliseconds: 700),
+        vsync: this,
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey[200]),
+            )
+        )
+            : null,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
+  }
+}
