65b294b2255efdf7ca3d010206fa1d7f983f4cf7
Nhan Nguyen <ngtrnhan1205@gmail.com>
2019-05-26 16:20:32 -0400

    Refactor some code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:friendlychat/model/chat_message.dart';
+import 'package:friendlychat/model/chat_screen.dart';
 import 'package:friendlychat/theme/theme_data.dart';
 
 const title = "Friendly Chat";
@@ -13,128 +13,12 @@ void main() {
 class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: title,
       theme: defaultTargetPlatform == TargetPlatform.iOS
           ? kIOSTheme
           : kDefaultTheme,
-      home: new ChatScreen(),
+      home: ChatScreen(title),
     );
-  }
-}
-
-class ChatScreen extends StatefulWidget {
-  @override
-  State createState() => new ChatScreenState();
-}
-
-class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
-  final List<ChatMessage> _messages = <ChatMessage>[];
-  final TextEditingController _textController = new TextEditingController();
-  bool _isComposing = false;
-
-  Widget _buildTextComposer() {
-    return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                child: new TextField(
-                  controller: _textController,
-                  onChanged: (String text) {
-                    setState(() {
-                      _isComposing = text.length > 0;
-                    });
-                  },
-                  onSubmitted: _handleSubmitted,
-                  decoration: new InputDecoration.collapsed(
-                      hintText: "Send a message"),
-                ),
-              ),
-              new Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                  child: Theme.of(context).platform == TargetPlatform.iOS ?
-                  CupertinoButton(
-                    child: Text("Send"),
-                    onPressed: _isComposing
-                        ? () =>  _handleSubmitted(_textController.text)
-                        : null,) :
-                  IconButton(
-                      icon: new Icon(Icons.send),
-                      onPressed: _isComposing
-                          ? () => _handleSubmitted(_textController.text)
-                          : null,
-                  )
-              )
-            ],
-          ),
-        )
-    );
-  }
-
-  void _handleSubmitted(String text) {
-    _textController.clear();
-
-    setState(() {
-      _isComposing = false;
-    });
-
-    ChatMessage message = new ChatMessage(
-      text: text,
-      animationController: new AnimationController(
-        duration: new Duration(milliseconds: 700),
-        vsync: this,
-      ),
-    );
-    setState(() {
-      _messages.insert(0, message);
-    });
-    message.animationController.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: ListView.builder(
-                padding: EdgeInsets.all(8.0),
-                reverse: true,
-                itemBuilder: (_, int index) => _messages[index],
-                itemCount: _messages.length,
-              ),
-            ),
-            Divider(height: 1.0),
-            Container(
-              decoration: BoxDecoration(
-                  color: Theme.of(context).cardColor),
-              child: _buildTextComposer(),
-            ),
-          ],
-        ),
-        decoration: Theme.of(context).platform == TargetPlatform.iOS
-          ? BoxDecoration(
-            border: Border(
-              top: BorderSide(color: Colors.grey[200]),
-            )
-          )
-          : null,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    for (ChatMessage message in _messages)
-      message.animationController.dispose();
-    super.dispose();
   }
 }
