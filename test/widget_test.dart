19f1863d4d6a9c3ed8f9ecc0ace2f7a4710bf3b1
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2019-06-09 22:37:47 -0400

    Migrate to AndroidX

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:friendlychat/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp());
+    await tester.pumpWidget(FriendlyChatApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
